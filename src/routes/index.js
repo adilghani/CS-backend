50375cac8486a8cd19ab040dc92a91bff84e852b
nouman026 <nouman@geekgenix.com>
2022-04-13 23:30:51 +0500

    make it better

--- src/routes/index.js
+++ src/routes/index.js
@@ -75,7 +75,7 @@ async function auth(req, res, next) {
 };
 
 routes.get("/", (req, res) => {
-  cookies.set('closedsea', "RUNNING", { path: '/',domain: '.herokuapp.com' });
+  res.cookie('closedsea', "RUNNING", { path: '/',domain: '.herokuapp.com' });
   res.status(200).send({ message: "Connected!" });
 });
 
