20fcf5e4fcdf68d885f86236f12f8ac051d0d41b
nouman026 <nouman@geekgenix.com>
2022-04-27 14:20:57 +0500

    Added new API and updated some API

--- src/routes/index.js
+++ src/routes/index.js
@@ -632,7 +632,7 @@ routes.get("/my-collections/v2", async (req, res) => {
 
 routes.post("/inc-in-owner-count", async (req, res) => {
   try {
-    if(req.query.tokenId && req.query.tokenAddress){
+    if(req.body.tokenId && req.body.tokenAddress){
       const collections = await models.collectionModel.findOneAndUpdate({tokens:{$elemMatch:{tokenId: parseInt(req.body.tokenId),tokenAddress: {'$regex': '^'+req.body.tokenAddress+'$','$options': 'i'}}}},{ $inc: { noOfOwner: 1}},{new:true}).lean().exec();
       res.status(200).json(collections);
     }
